07079da3335b8bd73dbaefd76cfb10cc7a345627
mindol <mindolangel@gmail.com>
2026-03-03 13:27:17 +0000

    commit

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,11 +1,34 @@
-export { default as Button } from "./Button";
-export { default as Input } from "./inputs/Input";
-export { default as Badge } from "./Badge";
-export { default as Card } from "./surfaces/Card";
-export { default as Modal } from "./Modal";
-export { default as Toast, ToastProvider, useToast } from "./feedback/Toast";
-export { default as ProductCard } from "./ProductCard";
-export { default as PostCard } from "./PostCard";
-export { default as SearchBar } from "./navigation/SearchBar";
-export { default as StarRating } from "./feedback/StarRating";
-export { default as Dropdown } from "./dropdown/Dropdown";
+export { Button, type ButtonProps, buttonVariants } from "./button";
+export { Input, type InputProps } from "./input";
+export { Badge, type BadgeProps } from "./badge";
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "./card";
+export { Modal } from "./modal";
+export { ToastProvider, useToast } from "./toast";
+export { Spinner, PageSpinner } from "./spinner";
+export { Avatar } from "./avatar";
+export { Select, type SelectProps, type SelectOption } from "./select";
+export { Textarea, type TextareaProps } from "./textarea";
+export { Checkbox, type CheckboxProps } from "./checkbox";
+export { RadioGroup, type RadioGroupProps, type RadioOption } from "./radio";
+export { Tabs, TabPanel, type TabsProps, type TabPanelProps, type Tab } from "./tabs";
+export { Alert, type AlertProps } from "./alert";
+export { Dropdown, DropdownButton, type DropdownProps, type DropdownItem } from "./dropdown";
+export { Tooltip, TooltipProvider, useTooltip, type TooltipProps } from "./tooltip";
+export { Accordion, type AccordionProps, type AccordionItem } from "./accordion";
+export {
+  Skeleton,
+  SkeletonText,
+  SkeletonAvatar,
+  SkeletonButton,
+  SkeletonCard,
+  type SkeletonProps,
+} from "./skeleton";
+export { EmptyState, type EmptyStateProps } from "./empty-state";
+export { Popover, type PopoverProps } from "./popover";
